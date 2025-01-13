1f2389ddac814ea5eec22b89abcd95b77224c456
syou551 <hiroto.0108si+@gmail.com>
2025-01-13 19:14:58 +0900

    fix site label position

--- nextjs-dashboard/src/app/ui/blogcard.tsx
+++ nextjs-dashboard/src/app/ui/blogcard.tsx
@@ -16,12 +16,13 @@ export default function BlogCard(
     ): JSX.Element{
 
     return(
-        <div className="flex transition justify-left items-left md:h-full md:h-5/6 bg-gray-100 hover:bg-blue-100 rounded-md shadow-md mr-5 ml-5 mb-5  hover:translate-y hover:scale-105">
-            <Link href={href} className="grid w-full " target="_blank">
-                <div className="flex mr-5 ml-5 justify-left items-left">
+        <div className="flex transition justify-left items-left md:h-5/6 bg-gray-100 hover:bg-blue-100 rounded-md shadow-md mr-5 ml-5 mb-5  hover:translate-y hover:scale-105">
+            <Link href={href} className="relative grid w-full " target="_blank">
+                <div className="flex mr-5 ml-5 justify-left items-left h-full">
                     <div className="ont-mono text-lg mt-5">{Title}</div>
                 </div>
-                <div className="flex ml-3 mr-3 items-left py-2">
+                <div className="flex relative ml-3 mr-3 items-left py-5 h-full">
+                    <div className="flex absolute bottom-2">
                         <p className={clsx("ont-mono mt-3",{
                          'bg-green-200' : description == 'Qiita' ,
                          'bg-gray-300'  : description == 'note'
@@ -29,6 +30,7 @@ export default function BlogCard(
                          "rounded-md px-2")}> {description} </p>
                         <p className="ont-mono mt-3 px-2 text-gray-400"> {date} </p>
                     </div>
+                </div>
             </Link>
         </div>
     )
